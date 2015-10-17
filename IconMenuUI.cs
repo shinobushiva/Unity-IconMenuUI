18e2027122a52373adaf844fdb2da1d396ab883f
Shinobu Izumi <izumi@cis.sojo-u.ac.jp>
2015-10-17 12:37:33 +0900

    first

---
+++ IconMenuUI.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Linq;
+
+public class IconMenuUI : MonoBehaviour
+{
+
+	public GameObject togglePrefab;
+	public RectTransform parentRect;
+
+	[System.Serializable]
+	public class IconMenu
+	{
+		public Canvas canvas;
+		public Toggle toggle;
+	} 
+
+	public IconMenu[] iconMenus;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+        
+        foreach (IconMenu im in iconMenus) {
+            im.toggle.isOn = false;
+			im.toggle.onValueChanged.AddListener(
+				(b)=>{ToggleCanvas(im.toggle);}
+			);
+
+            im.canvas.gameObject.SetActive(false);
+            im.canvas.enabled = false;
+        }
+
+	}
+
+    private IconMenu current;
+
+	public void ToggleCanvas (Toggle t)
+	{ 
+        if (current != null)
+        {
+            current.toggle.isOn = false;
+            current.canvas.gameObject.SetActive(false);
+            current.canvas.enabled = false;
+            current = null;
+        }
+
+		foreach (IconMenu im in iconMenus) {
+			if(im == null)
+				continue;
+			if(im.canvas == null)
+				continue;
+
+			if (im.toggle.isOn) {
+                current = im;
+				im.canvas.gameObject.SetActive(true);
+				im.canvas.enabled = true;
+			}
+		}
+
+
+
+	}
+}
